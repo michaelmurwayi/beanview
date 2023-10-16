adfd8324d1a26c77d6655d5860266b262d32a0f0
Michael Murwayi <mikemurwayi7@gmail.com>
2023-10-16 09:54:02 -0400

    ft-models:Adding grade model

--- app/api/models.py
+++ app/api/models.py
@@ -67,3 +67,9 @@ class Status(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+class Grade(models.Model):
+    id = models.AutoField(primary_key=True)
+    grade = models.CharField(max_length=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
