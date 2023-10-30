c9ca0caf42ffa6bfd020181045836c8ee89aa18c
Michael Murwayi <mikemurwayi7@gmail.com>
2023-10-30 08:00:52 -0400

    ft-dashboard: Adding global initail state constant

---
+++ Dashboard/src/components/State/initialState.js
@@ -0,0 +1,17 @@
+
+const State = {
+    totalNetWeight: "",
+    totalTareWeight: "",
+    totalBags: "",
+    totalUsers: "",
+    grades: [],
+    totalWeightPerGrade: [],
+    totalWeightPerWeek: [],
+    totalWeightPerMonth: [],
+    months: [],
+    dailyDeliveries: []
+
+}
+
+
+export default State
