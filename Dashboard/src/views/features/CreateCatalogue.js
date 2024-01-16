6bd623d41ac3b78485ef97c46f0dbab68834c9b3
Michael Murwayi <mikemurwayi7@gmail.com>
2024-01-16 08:39:12 -0500

    ft-dashboard: Add calendar filter for creating catalogue

--- Dashboard/src/views/features/CreateCatalogue.js
+++ Dashboard/src/views/features/CreateCatalogue.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React,  { useEffect, useState } from 'react';
 import { MDBDataTable } from 'mdbreact';
 import { connect } from 'react-redux';
 import { fetch_coffee_records } from 'components/State/action';
@@ -9,9 +9,9 @@ import { Calendar } from 'react-date-range';
 import "./catalogue.css"
 
 const DataTable = (props) => {
+  const {coffeeRecords, fetch_coffee_records} = props
+  const [filteredRecords, setFilteredRecords] = useState([])
   
-  const { coffeeRecords, fetch_coffee_records } = props
-
   useEffect (()=>{
     fetch_coffee_records();
   },[fetch_coffee_records])
@@ -30,6 +30,7 @@ const DataTable = (props) => {
         sort: 'asc',
         width: 250
       },
+
       {
         label: 'status',
         field: 'status',
@@ -43,13 +44,13 @@ const DataTable = (props) => {
   const data = {
     columns: [
       {
-        label: 'Lot id',
+        label: 'Estate',
         field: 'estate',
         sort: 'asc',
         width: 250
       },
       {
-        label: 'Lot Number',
+        label: 'Grade',
         field: 'grade',
         sort: 'asc',
         width: 250
@@ -96,37 +97,153 @@ const DataTable = (props) => {
         sort: 'asc',
         width: 100
       },
-      {
-        label: 'Status',
-        field: 'status',
-        sort: 'asc',
-        width: 100
-      }
+      
     ],
   
     rows: coffeeRecords
   
     
   }
-  const handleSelect = (event) =>{
+  const filteredData = {
+    columns: [
+      {
+        label: 'Estate',
+        field: 'estate',
+        sort: 'asc',
+        width: 250
+      },
+      {
+        label: 'Grade',
+        field: 'grade',
+        sort: 'asc',
+        width: 250
+      },
+      {
+        label: 'status',
+        field: 'status',
+        sort: 'asc',
+        width: 250
+      },
+      {
+        label: 'Bags',
+        field: 'bags',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'Outturn',
+        field: 'outturn',
+        sort: 'asc',
+        width: 150
+      },
+      {
+        label: 'Pockets',
+        field: 'pockets',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'Tare Weight',
+        field: 'tare_weight',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'Ticket',
+        field: 'ticket',
+        sort: 'asc',
+        width: 150
+      },
+      {
+        label: 'Variance',
+        field: 'variance',
+        sort: 'asc',
+        width: 100
+      },
+      
+    ],
   
+    rows: filteredRecords
   
+    
+  }
+  
+
+  function monthNameToNumber(monthName) {
+  const months = {
+    Jan: "01",
+    Feb: "02",
+    Mar: "03",
+    Apr: "04",
+    May: "05",
+    Jun: "06",
+    Jul: "07",
+    Aug: "08",
+    Sep: "09",
+    Oct: "10",
+    Nov: "11",
+    Dec: "12"
   };
 
+  return months[monthName];
+}
+
+
+  const filterDate =(date)=>{
+    const month = monthNameToNumber(date.split(" ").slice(1,4)[0])
+    const dates = date.split(" ").slice(1,4)[1]
+    const year = date.split(" ").slice(1,4)[2]
+
+    const newDate = year + "-" + month + "-" + dates
+    
+    return(newDate)
+  }
+  
+  const handleSelect = (event) =>{
+
+    setFilteredRecords(prevRecords => []);
+    
+    const startDate = filterDate(String(event.selection.startDate));
+    const endDate = filterDate(String(event.selection.endDate));
+    
+    if(coffeeRecords.length){
+      if(startDate == endDate){
+        const newRecords = coffeeRecords.map((record)=>{
+          if (record.created_at.split("T")[0] == startDate){
+            const newRecords =   [...filteredRecords, record]
+            setFilteredRecords(newRecords)        
+          }else{
+            const newRecords =   [...filteredRecords]
+            setFilteredRecords(newRecords)
+          }
+        })
+      }else{
+        
+      }
+    }else{
+      console.log(filteredData)
+    }
+
+  };
+
+  
   const selectionRange = {
     startDate: new Date(),
     endDate: new Date(),
     key: 'selection',
   }
+
   
   return (
     <div className='main'>
-    <div class="container">
-      <div class="column column1">
+    <div className="container">
+      <div className="column column1">
         <DateRangePicker
         ranges={[selectionRange]}
         onChange={handleSelect}
         className='calendar'
+        showTimeSelect={true}
+        
         />       
       </div>
       <div className="column column2">
@@ -146,7 +263,9 @@ const DataTable = (props) => {
         </div>
       </div>
   </div>
-    <div className='col-md-12'>
+  { filteredRecords.length === 0 &&
+
+      <div className='col-md-12'>
       <MDBDataTable
       hover
       data={data}
@@ -155,6 +274,18 @@ const DataTable = (props) => {
       searching= {false}
       />
     </div>
+    }
+  { filteredRecords.length > 0 &&
+    <div className='col-md-12'>
+    <MDBDataTable
+    hover
+    data={filteredData}
+    striped
+    small
+    searching= {false}
+    />
+  </div>
+  } 
   </div>
   );
 }
