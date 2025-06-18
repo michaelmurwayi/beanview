7a8208c3cda117f7b9c109d30a23faea9123ca98
michael murwayi <mikemurwayi7@gmail.com>
2025-06-18 08:58:21 -0500

    ft-navigation: Adding coffee navigation data to initial state

--- Dashboard/src/store/initialState.js
+++ Dashboard/src/store/initialState.js
@@ -13,7 +13,18 @@ export const globalInitialState = {
             { name: "Farmers Records", link: "/farmers", iconKey: "list" },
             { name: "Stock Summaries", link: "/farmers/stock-summaries", iconKey: "file" }
           ]
-        }
+        },
+       {
+          name: "Coffee",
+          iconKey: "coffee",
+          emoji: "☕",
+          isActive: (pathname) => pathname.startsWith("/products"),
+          dropdown: [
+            { name: "Add Coffee", link: "/coffee/add", iconKey: "add" },
+            { name: "View Coffee Records", link: "/coffee/view", iconKey: "list" },
+            { name: "Sale Summary", link: "/coffee/summary", iconKey: "file" }
+          ]
+        },
       ]
     }
   };
